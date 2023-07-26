2d55a2d34f5887d8cda7d03a86b478815d951b0b
AnthonySinitsa <itonyemail@gmail.com>
2023-07-26 22:27:21 +0000

    command

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # not clouds
+
+Do or don't use command idk, i think it works :/
+
+- gunicorn not_clouds_project.wsgi
--- not_clouds/apps.py
+++ not_clouds/apps.py
@@ -1,6 +1,16 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class NotCloudsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'not_clouds'
+# class NotCloudsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'not_clouds'
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
