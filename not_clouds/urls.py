b56056972363537a68a2f9db4db3c309973c7d20
AnthonySinitsa <itonyemail@gmail.com>
2023-07-26 22:19:03 +0000

    command

--- not_clouds/urls.py
+++ not_clouds/urls.py
@@ -3,5 +3,5 @@ from .views import NotCloudsList, NotCloudsDetail
 
 urlpatterns = [
   path('', NotCloudsList.as_view(), name='not_clouds_list'),
-  path('<int:pk>/', NotCloudsDetail.as_view(), name='not_clouds_detail'),
+  path('<int:pk>', NotCloudsDetail.as_view(), name='not_clouds_detail'),
 ]
