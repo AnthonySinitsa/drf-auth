2c28c2377e978ef54337edc34ff7cacc720b5601
AnthonySinitsa <itonyemail@gmail.com>
2023-07-26 21:01:50 +0000

    command

--- not_clouds/models.py
+++ not_clouds/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth import get_user_model
 
 class NotCloud(models.Model):
   owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-  title = models.CharField(max_length=64)
+  name = models.CharField(max_length=64)
   description = models.TextField(default='')
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   
   def __str__(self):
-    return self.title
+    return self.name
--- not_clouds/views.py
+++ not_clouds/views.py
@@ -3,11 +3,11 @@ from .serializer import NotCloudSerializer
 from .models import NotCloud
 from .permissions import IsOwnerOrReadOnly
 
-class NotCloudList(generics.ListCreateAPIView):
+class NotCloudsList(generics.ListCreateAPIView):
   queryset = NotCloud.objects.all()
   serializer_class = NotCloudSerializer
   
-class NotCloudDetail(generics.RetrieveUpdateDestroyAPIView):
+class NotCloudsDetail(generics.RetrieveUpdateDestroyAPIView):
   permission_classes = (IsOwnerOrReadOnly,)
   queryset = NotCloud.objects.all()
   serializer_class = NotCloudSerializer
